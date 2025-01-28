82b2610c2b07c02cfad5fade6bfdff3a728cd13e
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-28 10:17:19 -0500

    Feat: Make scoreboard and add title 'pokecards!'

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import '@styles/header.css'
+
+function Header({ score = 0, bestScore = 0 }) {
+    return (
+        <>
+            <header>
+                <nav>
+                    <h1><a href="/">PokeCards!</a></h1>
+                    <div className="scoreBoard-container">
+                        <p>Score: <span>{score}</span></p>
+                        <p>Best Score: <span>{bestScore}</span></p>
+                    </div>
+                </nav>
+            </header>
+            <section>
+                <h2>
+                    Welcome to PokeCards!
+                </h2>
+                <p>
+                    Don&apos;t click on the same card twice!
+                </p>
+            </section>
+        </>
+    )
+}
+
+export default Header;
