516e9444e8fbddd0cad53ce5b3bfc23dddbc9213
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-28 10:13:32 -0500

    Import Card, Header and Footer, also make random numbers to generate random pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import '@styles/App.css'
 import Card from '@components/Card'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+const handleRandom = () => {
+  const randomNumber = Math.floor(Math.random() * 150) + 1
+  return randomNumber
+}
+
+const pokemonList = [handleRandom(), handleRandom(), handleRandom(), handleRandom(), handleRandom(), handleRandom(), handleRandom(), handleRandom(), handleRandom(), handleRandom()]
 
 function App() {
+
   return (
     <>
-      <Card/>
+    <Header/>
+    <div className="card-container">
+      {pokemonList.map((element, index) => {
+        return <Card pokemon={element} key={index}/>
+      })}
+    </div>
+    <Footer/>
     </>
   )
 }
